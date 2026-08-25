435dc21581f292cb081c6b758e82d28a776cf4ab
sanjaypoojary56-art <sanjaypoojary56@gmail.com>
2026-08-25 17:58:03 +0530

    Remove controller-level CORS annotation

--- src/main/java/com/example/demo/controllers/dashboardcontroller.java
+++ src/main/java/com/example/demo/controllers/dashboardcontroller.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +22,6 @@ import java.util.List;
 @RestController
 public class dashboardcontroller {
     private DashboardService dbs;
-    @CrossOrigin(origins = "http://localhost:5173")
     @RequestMapping("/dashboard")
     @PreAuthorize("hasRole('STUDENT')")
     public DashboardRequest show(Authentication authentication)
