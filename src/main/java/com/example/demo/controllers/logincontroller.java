c3cf4fea36fbbe486e8636efeccc1501b54d1f53
sanjaypoojary56-art <sanjaypoojary56@gmail.com>
2026-08-25 18:11:07 +0530

    Remove controller-level CORS annotation

--- src/main/java/com/example/demo/controllers/logincontroller.java
+++ src/main/java/com/example/demo/controllers/logincontroller.java
@@ -4,7 +4,6 @@ import com.example.demo.LoginRequest.LoginRequest;
 import com.example.demo.Responses.logres;
 import com.example.demo.services.loginservice;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class logincontroller {
     @Autowired
     private loginservice ls;
-    @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping("/login")
     public logres loginHandler(@RequestBody LoginRequest re)
     {
