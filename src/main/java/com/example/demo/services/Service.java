108c62c43deac29ca8604526df9c42c8b71009eb
sanjaypoojary56-art <sanjaypoojary56@gmail.com>
2026-08-25 23:51:01 +0530

    Update Service.java

--- src/main/java/com/example/demo/services/Service.java
+++ src/main/java/com/example/demo/services/Service.java
@@ -1,10 +1,8 @@
 package com.example.demo.services;
 
-import com.example.demo.Repositories.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @org.springframework.stereotype.Service
 public class Service {
-    @Autowired
-    private Repository rep;
+    
 }
